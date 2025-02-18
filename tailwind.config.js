894f18d1758edec85fb337ce2d4f0d5b57376862
Jessica <jessica.troilo25@gmail.com>
2025-02-18 14:11:51 +0100

    config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     "./src/**/*.php",
   ],
   theme: {
+    colors: {
+      'matcha': '#A7C957',
+      'matcha-accent': '#8AB545',
+      'grey-lavender': '#D7D7F2',
+      'pink-watermelon': '#FF6B95',
+      'text': '#464242',
+      'footer-text': '#FFF8E2',
+    },
     extend: {},
   },
   plugins: [],
