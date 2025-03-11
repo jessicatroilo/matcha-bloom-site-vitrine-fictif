563335d10636eb0886e940d27207d596cde40973
jessicatroilo <jessica.troilo25@gmail.com>
2025-03-11 12:16:29 +0100

    feat(templates-src) : create json for product - create service - render dynamic twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\ProductHomeService;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductHomeService $productHomeService): Response
     {
+        $productsInHomePage = $productHomeService->getProducts();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $productsInHomePage,
         ]);
     }
 }
