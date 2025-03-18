4b51acbfdea210d119340758291603657bcec1a3
jessicatroilo <jessica.troilo25@gmail.com>
2025-03-18 17:28:30 +0100

    security form ok

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,8 +2,10 @@
 
 namespace App\Form;
 
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -24,7 +26,23 @@ class ContactType extends AbstractType
                 ],
                 'attr' => ['class' => 'form-input', 'size' => 10],
                 'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le nom ne peut pas être vide.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 100,
+                        'min' => 2,
+                        'minMessage' => 'Le nom doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le nom ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-ZÀ-ÿ -]+$/', // Permet uniquement des lettres, des accents, des espaces et des tirets
+                        'message' => 'Le nom ne peut contenir que des lettres, des accents, des espaces et des tirets.',
+                    ]),
+                ]
             ])
+            
 
             ->add('email', EmailType::class, [
                 'label' => 'Adresse email *',
@@ -35,6 +53,20 @@ class ContactType extends AbstractType
                 'label_attr' => ['class' => 'form-label'],
                 'attr' => ['class' => 'form-input', 'size' => 50],
                 'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'L\'adresse email ne peut pas être vide.',
+                    ]),
+                    new Assert\Email([
+                        'message' => 'L\'adresse email {{ value }} n\'est pas valide.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 100,
+                        'min'=> 5,
+                        'minMessage' => 'L\'adresse email doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'L\'adresse email ne peut pas dépasser {{ limit }} caractères.',
+                    ]),
+                ],
             ])
 
             ->add('subject', ChoiceType::class, [
@@ -51,6 +83,15 @@ class ContactType extends AbstractType
                 ],
                 'attr' => ['class' => 'form-input'],
                 'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le sujet ne peut pas être vide.',
+                    ]),
+                    new Assert\Choice([
+                        'choices' => ['Question générale', 'Demande d\'informations', 'Réservation', 'Autre'],
+                        'message' => 'Le sujet n\'est pas valide.',
+                    ]),
+                ],
             ])
 
             ->add('message', TextareaType::class, [
@@ -60,18 +101,35 @@ class ContactType extends AbstractType
                     'aria-label' => 'Message (champ obligatoire)',
                 ],
                 'attr' => ['class' => 'form-input', 'rows' => 4],
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le message ne peut pas être vide.',
+                    ]),
+                    new Assert\Length([
+                        'max' => 1000,
+                        'min' => 5,
+                        'minMessage' => 'Le message doit contenir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le message ne peut pas dépasser {{ limit }} caractères.',
+                        
+                    ]),
+                ],
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Envoyer',
                 'attr' => [
                     'class' => 'form-btn',
                     'aria-label' => 'Bouton pour envoyer le message',
-                ]
+                ],
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([]);
+        $resolver->setDefaults([
+            'csrf_protection' => true, // Assure que CSRF est activé
+            'csrf_field_name' => '_token', // Le nom du champ CSRF
+            'csrf_token_id' => 'contact_form', // Identifiant du token
+        ]);
     }
 }
